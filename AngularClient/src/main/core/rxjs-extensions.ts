44e76ee0d1e6e8517c4d7bc36ad1e48f45972491
Serkan Holat <coni2k@users.noreply.github.com>
2018-07-04 20:02:28 +0200

    0.6.7

--- AngularClient/src/main/core/rxjs-extensions.ts
+++
@@ -1,8 +0,0 @@
-import "rxjs/add/operator/catch";
-import "rxjs/add/operator/finally";
-import "rxjs/add/operator/mergeMap";
-import "rxjs/add/operator/share";
-
-import "rxjs/add/observable/of";
-import "rxjs/add/observable/fromPromise";
-import "rxjs/add/observable/timer";
