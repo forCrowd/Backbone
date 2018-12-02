fed86099fc8950c4fa3cc2c944a2f5391a7dc932
Serkan Holat <coni2k@users.noreply.github.com>
2018-12-02 10:22:27 +0100

    .

--- AngularClient/projects/backbone-client-core/src/lib/app-entity-manager/app-entity-manager-config.ts
+++ AngularClient/projects/backbone-client-core/src/lib/app-entity-manager/app-entity-manager-config.ts
@@ -1,35 +1,34 @@
 import {
-  ElementCell, ElementField, ElementItem, Element, Project, Role, User, UserClaim, UserElementCell,
+  Element, ElementCell, ElementField, ElementItem, Project, Role, User, UserClaim, UserElementCell,
   UserElementField, UserLogin, UserRole
 } from "../entities";
 
 export class EntityManagerConfig {
 
-  constructor(public elementCellType?: any,
+  constructor(public elementType?: any,
+    public elementCellType?: any,
     public elementFieldType?: any,
     public elementItemType?: any,
-    public elementType?: any,
     public projectType?: any,
     public roleType?: any,
+    public userType?: any,
     public userClaimType?: any,
     public userElementCellType?: any,
     public userElementFieldType?: any,
     public userLoginType?: any,
-    public userRoleType?: any,
-    public userType?: any) {
+    public userRoleType?: any) {
 
+    if (!this.elementType) this.elementType = Element;
     if (!this.elementCellType) this.elementCellType = ElementCell;
     if (!this.elementFieldType) this.elementFieldType = ElementField;
     if (!this.elementItemType) this.elementItemType = ElementItem;
-    if (!this.elementType) this.elementType = Element;
     if (!this.projectType) this.projectType = Project;
     if (!this.roleType) this.roleType = Role;
+    if (!this.userType) this.userType = User;
     if (!this.userClaimType) this.userClaimType = UserClaim;
     if (!this.userElementCellType) this.userElementCellType = UserElementCell;
     if (!this.userElementFieldType) this.userElementFieldType = UserElementField;
     if (!this.userLoginType) this.userLoginType = UserLogin;
     if (!this.userRoleType) this.userRoleType = UserRole;
-    if (!this.userType) this.userType = User;
-
   }
 }
